7d23b6fb3cced43ecc7376bc07e78125c7629935
yanislammari <yanis.lammari937@gmail.com>
2024-11-11 01:04:55 +0100

    add connect to postgresql database

--- src/app.py
+++ src/app.py
@@ -1,6 +1,15 @@
 from flask import Flask
+from src.utils.config import db
+from src.utils.config.config import Config
 
 
 def create_app():
     app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+
+    with app.app_context():
+        db.create_all()
+
     return app
