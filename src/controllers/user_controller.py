fe0c0ccc0fd5ae2f4cf73c8902c286d8a0d31c07
yanislammari <yanis.lammari937@gmail.com>
2024-11-12 06:27:52 +0100

    add put user http request

--- src/controllers/user_controller.py
+++ src/controllers/user_controller.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify, abort
+from flask import Blueprint, request, jsonify, abort
 from src.services.user_service import UserService
 
 user_blueprint = Blueprint("user", __name__)
@@ -27,4 +27,26 @@ def get_user(id):
         "username": user.username,
         "mail": user.mail,
         "password": user.password
+    })
+
+
+@user_blueprint.route("/users/<string:id>", methods=["PUT"])
+def update_user(id):
+    if not UserService.get_user_by_id(id):
+        abort(404, description="User not found.")
+
+    data = request.get_json()
+    if not data:
+        abort(400, description="No data provided.")
+
+    try:
+        updated_user = UserService.set_user_by_id(id, data)
+    except ValueError as e:
+        abort(400, description=str(e))
+
+    return jsonify({
+        "id": str(updated_user.id),
+        "username": updated_user.username,
+        "mail": updated_user.mail,
+        "password": updated_user.password
     })
